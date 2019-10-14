10d406f60c6c9662e695ff1236b2536ba4c1c8b4
Oliver Caldwell <ollie@oli.me.uk>
2019-10-14 13:31:00 +0100

    Initial templating and style work

--- src/blog/main.clj
+++ src/blog/main.clj
@@ -1,23 +1,41 @@
 (ns blog.main
   (:require [me.raynes.fs :as fs]
+            [selmer.parser :as tmpl]
             [blog.adoc :as adoc]))
 
 (defn- spit-post!
   "Write the given post to the output directory under the appropriate name.
   Assuming it's been run through adoc/parse already and contains the resulting keys."
-  [{:keys [file html]}]
-  (let [output-dir (fs/file "output" (fs/name file))]
+  [{:keys [slug html]}]
+  (let [output-dir (fs/file "output" slug)]
     (fs/mkdirs output-dir)
     (spit (str (fs/file output-dir "index.html")) html)))
 
+(defn- tmpl
+  "Render a template within the base template with the provided options."
+  [tmpl-name opts]
+  (tmpl/render-file
+    "base.html"
+    (assoc opts
+           :body (tmpl/render-file
+                   (str tmpl-name ".html")
+                   opts))))
+
 (defn -main []
+  (fs/delete-dir "output")
+  (fs/mkdirs "output")
   (->> (fs/list-dir "posts")
-       (sequence
-         (comp
-           (map (juxt identity slurp))
-           (map #(zipmap [:file :source] %))
-           (map #(merge % (adoc/parse (:source %))))))
-       (run! spit-post!)))
+       (map (fn [file]
+              (-> (adoc/parse (slurp file))
+                  (assoc :slug (fs/name file)))))
+       (map (fn [{:keys [title] :as post}]
+              (-> post
+                  (update :html
+                          #(tmpl "post"
+                                 {:title title
+                                  :content %})))))
+       (run! spit-post!))
+  (fs/copy-dir "assets" "output"))
 
 (comment
   (-main))
