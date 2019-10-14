6de0dd9ab7880ad8efdf5f3cd4c2d525dc755103
Oliver Caldwell <ollie@oli.me.uk>
2019-10-14 18:14:50 +0100

    Use a base dir for the default output
    Can be used to keep a committed .htaccess or something.

--- src/blog/main.clj
+++ src/blog/main.clj
@@ -3,13 +3,17 @@
             [selmer.parser :as tmpl]
             [blog.adoc :as adoc]))
 
+(def ^:private output-dir (fs/file "output"))
+(def ^:private posts-dir (fs/file "posts"))
+(def ^:private base-dir (fs/file "base"))
+
 (defn- spit-post!
   "Write the given post to the output directory under the appropriate name.
   Assuming it's been run through adoc/parse already and contains the resulting keys."
   [{:keys [slug html]}]
-  (let [output-dir (fs/file "output" slug)]
-    (fs/mkdirs output-dir)
-    (spit (str (fs/file output-dir "index.html")) html)))
+  (let [prefix (fs/file output-dir slug)]
+    (fs/mkdirs prefix)
+    (spit (str (fs/file prefix "index.html")) html)))
 
 (defn- tmpl
   "Render a template within the base template with the provided options."
@@ -21,21 +25,22 @@
                    (str tmpl-name ".html")
                    opts))))
 
-(defn -main []
-  (fs/delete-dir "output")
-  (fs/mkdirs "output")
-  (->> (fs/list-dir "posts")
-       (map (fn [file]
-              (-> (adoc/parse (slurp file))
-                  (assoc :slug (fs/name file)))))
-       (map (fn [{:keys [title] :as post}]
-              (-> post
-                  (update :html
-                          #(tmpl "post"
-                                 {:title title
-                                  :content %})))))
-       (run! spit-post!))
-  (fs/copy-dir "assets" "output"))
+(defn -main
+  "Performs all building of the blog from source."
+  []
+  (fs/delete-dir output-dir)
+  (fs/copy-dir base-dir output-dir)
+  (let [posts (->> (fs/list-dir posts-dir)
+                   (map (fn [file]
+                          (-> (adoc/parse (slurp file))
+                              (assoc :slug (fs/name file)))))
+                   (map (fn [{:keys [title] :as post}]
+                          (-> post
+                              (update :html
+                                      #(tmpl "post"
+                                             {:title title
+                                              :content %}))))))]
+    (run! spit-post! posts)))
 
 (comment
-  (-main))
+  (time (-main)))
