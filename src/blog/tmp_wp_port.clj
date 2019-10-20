28ffb9f20cab374464b038486a38673d0d36d2e1
Oliver Caldwell <ollie@oli.me.uk>
2019-10-20 14:05:32 +0100

    First pass at converting old WP posts to adoc

--- src/blog/tmp_wp_port.clj
+++ src/blog/tmp_wp_port.clj
@@ -1,7 +1,9 @@
 (ns blog.tmp-wp-port
   (:require [clojure.string :as str]
+            [clojure.java.shell :as shell]
             [hickory.core :as html]
             [hickory.select :as css]
+            [hickory.render :as render]
             [me.raynes.fs :as fs]))
 
 (defn legacy-post-paths [root]
@@ -21,8 +23,33 @@
      :date (-> (css/select (css/class "entry-date") dom)
                (get-in [0 :attrs :datetime])
                (str/replace #"T.*" ""))
-     #_#_:content (css/select (css/class "entry-content") dom)}))
+     :slug (fs/name (fs/parent path))
+     :content (-> (css/select (css/class "entry-content") dom)
+                  (first)
+                  (update
+                    :content
+                    (fn [nodes]
+                      (map (fn [{:keys [attrs] :as node}]
+                             (if (some-> (:class attrs) (str/includes? "crayon-syntax"))
+                               (-> (css/select (css/class "crayon-plain") node)
+                                   (first)
+                                   (assoc :tag :pre))
+                               node))
+                           nodes)))
+                  (render/hickory-to-html)
+                  (as-> $
+                    (shell/sh "pandoc" "--wrap=none" "-f" "html" "-t" "asciidoc"
+                              :in (.getBytes $))
+                    (get $ :out)))}))
 
-(-> (legacy-post-paths "../olical.github.io")
-    (first)
-    (->post-data))
+(defn write-post! [{:keys [title date content slug]}]
+  (spit (fs/file "posts" (str slug ".adoc"))
+        (str "= " title "\n"
+             "Oliver Caldwell\n"
+             date "\n\n"
+             content)))
+
+(comment
+  (->> (legacy-post-paths "../olical.github.io")
+       (map ->post-data)
+       (run! write-post!)))
